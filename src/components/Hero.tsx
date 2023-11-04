49ee976c420043aecf3199fdeb82ca63e73f4af6
Great Ochuko <greatochuko@gmail.com>
2023-11-04 09:41:09 +0100

    finished mobile styling for navbar

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,45 +1,11 @@
-import { Link, useLocation } from "react-router-dom";
-import { useState } from "react";
+import { Link } from "react-router-dom";
 import styles from "./Hero.module.css";
+import Navbar from "./Navbar";
 
 export default function Hero() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const { pathname } = useLocation();
   return (
     <header className={styles.header}>
-      <nav>
-        <img src="/logo.png" alt="logo" className={styles.logo} />
-        <ul>
-          <li className={pathname === "/" ? styles.active : ""}>
-            <Link to={"/menu"}>Menu</Link>
-          </li>
-          <li>
-            <Link to={"/vendors"}>Vendors</Link>
-          </li>
-          <li>
-            <Link to={"/categories"}>Categories</Link>
-          </li>
-          <li>
-            <Link to={"/cart"}>Cart</Link>
-          </li>
-          {isLoggedIn ? (
-            <li>
-              <Link to={"/order"}>Orders</Link>
-            </li>
-          ) : null}
-        </ul>
-        <div className={styles.user}>
-          {isLoggedIn ? (
-            <Link to={""} onClick={() => setIsLoggedIn(false)}>
-              Logout
-            </Link>
-          ) : (
-            <Link to={""} onClick={() => setIsLoggedIn(true)}>
-              Login
-            </Link>
-          )}
-        </div>
-      </nav>
+      <Navbar />
       <div className={styles.hero}>
         <div className={styles.heading}>
           <h1>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,58 @@
+import { Link, useLocation } from "react-router-dom";
+import styles from "./Navbar.module.css";
+import { useState } from "react";
+
+export default function Navbar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const { pathname } = useLocation();
+  const [navIsOpen, setNavIsOpen] = useState(false);
+
+  return (
+    <nav className={styles.navbar}>
+      <button
+        className={styles.toggleNavBtn}
+        onClick={() => setNavIsOpen(true)}
+      >
+        <i className="fa-solid fa-bars"></i>
+      </button>
+      <img src="/logo.png" alt="logo" className={styles.logo} />
+
+      <ul className={navIsOpen ? styles.open : ""}>
+        <li className={pathname === "/" ? styles.active : ""}>
+          <Link to={"/menu"}>Menu</Link>
+        </li>
+        <li>
+          <Link to={"/vendors"}>Vendors</Link>
+        </li>
+        <li>
+          <Link to={"/categories"}>Categories</Link>
+        </li>
+        <li>
+          <Link to={"/cart"}>Cart</Link>
+        </li>
+        {isLoggedIn ? (
+          <li>
+            <Link to={"/order"}>Orders</Link>
+          </li>
+        ) : null}
+      </ul>
+      <div
+        className={[styles.mobileOverlay, navIsOpen ? styles.open : null].join(
+          " "
+        )}
+        onClick={() => setNavIsOpen(false)}
+      ></div>
+      <div className={styles.user}>
+        {isLoggedIn ? (
+          <Link to={""} onClick={() => setIsLoggedIn(false)}>
+            Logout
+          </Link>
+        ) : (
+          <Link to={""} onClick={() => setIsLoggedIn(true)}>
+            Login
+          </Link>
+        )}
+      </div>
+    </nav>
+  );
+}
