2397ae94df75a6443d343099eac955eb0c3df39d
Great Ochuko <greatochuko@gmail.com>
2023-11-05 02:12:46 +0100

    relocated cart modal to cart button component

--- src/components/CartButton.tsx
+++ src/components/CartButton.tsx
@@ -1,12 +1,19 @@
 import styles from "./CartButton.module.css";
 import { useCartContext } from "../hooks/useCartContext";
+import Cart from "./Cart";
+import { useState } from "react";
 
 export default function CartButton() {
   const { cart } = useCartContext();
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <button className={styles.cartBtn}>
-      <i className="fa-solid fa-cart-shopping"></i>
-      <span>{cart.length}</span>
-    </button>
+    <>
+      <button className={styles.cartBtn} onClick={() => setIsOpen(true)}>
+        <i className="fa-solid fa-cart-shopping"></i>
+        <span>{cart.length}</span>
+      </button>
+      <Cart isOpen={isOpen} setIsOpen={setIsOpen} />
+    </>
   );
 }
